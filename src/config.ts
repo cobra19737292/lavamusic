ff8457a11c90b2e2d5bb8dff910b2c94715f7cfa
cobra19737292 <gangadharraparthi009@gmail.com>
2026-08-29 13:35:06 +0530

    Update emoji definitions to use custom IDs
    Signed-off-by: cobra19737292 <gangadharraparthi009@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -7,31 +7,31 @@ export default {
 		main: 0x2f3136,
 	},
 	// You can add custom emoji with ID format (e.g., <:emojiName:123456789012345678>)
-	emoji: {
-		pause: "⏸️",
-		resume: "▶️",
-		stop: "⏹️",
-		skip: "⏭️",
-		previous: "⏮️",
-		forward: "⏩",
-		rewind: "⏪",
-		volume: {
-			down: "🔉",
-			up: "🔊",
-		},
-		shuffle: "🔀",
-		loop: {
-			none: "🔁",
-			track: "🔂",
-		},
-		page: {
-			last: "⏩",
-			first: "⏪",
-			back: "⬅️",
-			next: "➡️",
-			cancel: "⏹️",
-		},
-	},
+emoji: {
+    pause: "<a:pause:1543168651631530074>",
+    resume: "<a:currently_Playing:1543168593884086292>",
+    stop: "<a:neon_stop:1543168637500919938>",
+    skip: "<a:skip:1543168725190967377>",
+    previous: "<:previous:1543168666563252245>",
+    forward: "<:forward:1543168608371220594>",
+    rewind: "<:rewind:1543168681788575785>",
+    volume: {
+        down: "<a:volume_down:1543168767209512963>",
+        up: "<a:volume_up:1543168776248492072>",
+    },
+    shuffle: "<:shuffle:1543168718002069560>",
+    loop: {
+        none: "<a:loop:1543168629955362858>",
+        track: "<a:loop:1543168629955362858>",
+    },
+    page: {
+        last: "<:forward:1543168608371220594>",
+        first: "<:rewind:1543168681788575785>",
+        back: "<:previous:1543168666563252245>",
+        next: "<:forward:1543168608371220594>",
+        cancel: "<a:neon_stop:1543168637500919938>",
+    },
+},
 	icons: {
 		youtube: "https://i.imgur.com/xzVHhFY.png",
 		spotify: "https://i.imgur.com/qvdqtsc.png",
