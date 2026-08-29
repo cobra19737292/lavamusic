ea00ef4188d1143668b919372b2386def20f7aff
cobra19737292 <gangadharraparthi009@gmail.com>
2026-08-29 13:45:25 +0530

    Add files via upload
    Signed-off-by: cobra19737292 <gangadharraparthi009@gmail.com>

---
+++ src/config.ts
@@ -0,0 +1,46 @@
+export default {
+	color: {
+		red: 0xff0000,
+		green: 0x00ff00,
+		blue: 0x0000ff,
+		yellow: 0xffff00,
+		main: 0x2f3136,
+	},
+	// You can add custom emoji with ID format (e.g., <:emojiName:123456789012345678>)
+	emoji: {
+		pause: "⏸️",
+		resume: "▶️",
+		stop: "⏹️",
+		skip: "⏭️",
+		previous: "⏮️",
+		forward: "⏩",
+		rewind: "⏪",
+		volume: {
+			down: "🔉",
+			up: "🔊",
+		},
+		shuffle: "🔀",
+		loop: {
+			none: "🔁",
+			track: "🔂",
+		},
+		page: {
+			last: "⏩",
+			first: "⏪",
+			back: "⬅️",
+			next: "➡️",
+			cancel: "⏹️",
+		},
+	},
+	icons: {
+		youtube: "https://i.imgur.com/xzVHhFY.png",
+		spotify: "https://i.imgur.com/qvdqtsc.png",
+		soundcloud: "https://i.imgur.com/MVnJ7mj.png",
+		applemusic: "https://i.imgur.com/Wi0oyYm.png",
+		deezer: "https://i.imgur.com/xyZ43FG.png",
+		jiosaavn: "https://i.imgur.com/N9Nt80h.png",
+	} as any,
+	links: {
+		img: "https://i.imgur.com/ud3EWNh.jpg",
+	},
+};
